37b1cd37ba05629f7793650a98ea1bcd983a8828
Denis Pavlyutchenko <Hixon10@yandex.ru>
2015-09-18 00:11:03 +0300

    GrepCLI is implemented

--- design-task-1/src/main/java/ru/spbau/pavlyutchenko/task1/Grep.java
+++ design-task-1/src/main/java/ru/spbau/pavlyutchenko/task1/Grep.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.util.*;
 import java.util.regex.Pattern;
 
-@Command(name = "grep")
+@Command(name = "grepHelper")
 public class Grep implements ICommand {
 
     @Override
@@ -34,67 +34,7 @@ public class Grep implements ICommand {
                 }
             }
 
-
-            List<String> lines = getLines(args, isFirstCommand);
-
-            ArrayList<String> resultLines = new ArrayList<>();
-            Set<Integer> addedLinesIndex = new HashSet<>();
-
-            String query = args[args.length - 2];
-            Pattern re = null;
-
-            if (hasIgnoreCaseFlag) {
-                re = Pattern.compile(query, Pattern.CASE_INSENSITIVE);
-            } else {
-                re = Pattern.compile(query);
-            }
-
-
-            for (int j = 0; j < lines.size(); j++) {
-                String line = lines.get(j);
-                boolean find = false;
-
-                if (hasWordReFlag) {
-                    String[] splited = line.split("\\s+");
-
-                    for (int i = 0; i < splited.length; i++) {
-                        if (hasIgnoreCaseFlag) {
-                            splited[i] = splited[i].toLowerCase();
-                            query = query.toLowerCase();
-                        }
-
-                        if (query.equals(splited[i])) {
-                            find = true;
-                            break;
-                        }
-                    }
-                } else {
-                    if (re.matcher(line).find()) {
-                        find = true;
-                    }
-                }
-
-                if (find) {
-                    if (hasAfterContextFlag) {
-                        int addedLeft = afterContextFlagValue;
-
-                        for (int d = j; d < lines.size() && addedLeft >= 0; d++) {
-                            if (!addedLinesIndex.contains(d)) {
-                                resultLines.add(lines.get(d));
-                                addedLinesIndex.add(d);
-                            }
-                            addedLeft--;
-                        }
-                    } else {
-                        if (!addedLinesIndex.contains(j)) {
-                            resultLines.add(line);
-                            addedLinesIndex.add(j);
-                        }
-                    }
-                }
-            }
-
-            return String.join(System.lineSeparator(), resultLines);
+            return grepHelper(args, isFirstCommand, hasWordReFlag, hasIgnoreCaseFlag, hasAfterContextFlag, afterContextFlagValue);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
@@ -102,7 +42,70 @@ public class Grep implements ICommand {
         return "";
     }
 
-    private List<String> getLines(String[] args, Boolean isFirstCommand) throws IOException {
+    public static String grepHelper(String[] args, Boolean isFirstCommand, boolean hasWordReFlag, boolean hasIgnoreCaseFlag, boolean hasAfterContextFlag, int afterContextFlagValue) throws IOException {
+        List<String> lines = getLines(args, isFirstCommand);
+
+        ArrayList<String> resultLines = new ArrayList<>();
+        Set<Integer> addedLinesIndex = new HashSet<>();
+
+        String query = args[args.length - 2];
+        Pattern re = null;
+
+        if (hasIgnoreCaseFlag) {
+            re = Pattern.compile(query, Pattern.CASE_INSENSITIVE);
+        } else {
+            re = Pattern.compile(query);
+        }
+
+
+        for (int j = 0; j < lines.size(); j++) {
+            String line = lines.get(j);
+            boolean find = false;
+
+            if (hasWordReFlag) {
+                String[] splited = line.split("\\s+");
+
+                for (int i = 0; i < splited.length; i++) {
+                    if (hasIgnoreCaseFlag) {
+                        splited[i] = splited[i].toLowerCase();
+                        query = query.toLowerCase();
+                    }
+
+                    if (query.equals(splited[i])) {
+                        find = true;
+                        break;
+                    }
+                }
+            } else {
+                if (re.matcher(line).find()) {
+                    find = true;
+                }
+            }
+
+            if (find) {
+                if (hasAfterContextFlag) {
+                    int addedLeft = afterContextFlagValue;
+
+                    for (int d = j; d < lines.size() && addedLeft >= 0; d++) {
+                        if (!addedLinesIndex.contains(d)) {
+                            resultLines.add(lines.get(d));
+                            addedLinesIndex.add(d);
+                        }
+                        addedLeft--;
+                    }
+                } else {
+                    if (!addedLinesIndex.contains(j)) {
+                        resultLines.add(line);
+                        addedLinesIndex.add(j);
+                    }
+                }
+            }
+        }
+
+        return String.join(System.lineSeparator(), resultLines);
+    }
+
+    private static List<String> getLines(String[] args, Boolean isFirstCommand) throws IOException {
         List<String> lines;
         if (isFirstCommand) {
             File file = new File(args[args.length - 1]);
@@ -115,6 +118,6 @@ public class Grep implements ICommand {
 
     @Override
     public String man() {
-        return "The grep command prints lines matching a pattern. This command accepts filename as argument.";
+        return "The grepHelper command prints lines matching a pattern. This command accepts filename as argument.";
     }
 }
---
+++ design-task-1/src/main/java/ru/spbau/pavlyutchenko/task1/GrepCli.java
@@ -0,0 +1,61 @@
+package ru.spbau.pavlyutchenko.task1;
+
+import org.apache.commons.cli.*;
+
+import java.io.IOException;
+
+@Command(name = "grepCli")
+public class GrepCli implements ICommand {
+
+    private final Options options;
+
+    public GrepCli() {
+        options = new Options();
+        options.addOption("i", false, "Ignore case flag");
+        options.addOption("w", false, "Word regexp");
+        options.addOption("A", true, "After context");
+    }
+
+    @Override
+    public String run(String[] args, Boolean isFirstCommand) {
+        try {
+            if (args.length < 3) {
+                System.out.println("There are no args for command ru.spbau.pavlyutchenko.task1.GrepCli");
+                return "";
+            }
+
+            CommandLineParser parser = new DefaultParser();
+            CommandLine cmd = parser.parse(options, args);
+
+            boolean hasWordReFlag = false;
+            boolean hasIgnoreCaseFlag = false;
+
+            boolean hasAfterContextFlag = false;
+            int afterContextFlagValue = 0;
+
+            if(cmd.hasOption("w")) {
+                hasWordReFlag = true;
+            }
+
+            if(cmd.hasOption("i")) {
+                hasIgnoreCaseFlag = true;
+            }
+
+            if(cmd.hasOption("A")) {
+                hasAfterContextFlag = true;
+                afterContextFlagValue = Integer.parseInt(cmd.getOptionValue("A"));
+            }
+
+            return Grep.grepHelper(args, isFirstCommand, hasWordReFlag, hasIgnoreCaseFlag, hasAfterContextFlag, afterContextFlagValue);
+        } catch (IOException | ParseException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return "";
+    }
+
+    @Override
+    public String man() {
+        return "The grepHelper command prints lines matching a pattern. This command accepts filename as argument.";
+    }
+}
